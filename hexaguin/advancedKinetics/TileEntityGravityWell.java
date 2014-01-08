998159db5ddcfead5f4ac37afcd53d6e364ec4b3
hexaguin <hexaguin@gmail.com>
2014-01-07 20:46:44 -0700

    Added gravity inverter and well.

--- hexaguin/advancedKinetics/TileEntityGravityWell.java
+++ hexaguin/advancedKinetics/TileEntityGravityWell.java
@@ -12,23 +12,34 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
-public class TileEntityGravityInverter extends TileEntity {
+public class TileEntityGravityWell extends TileEntity {
 	public void updateEntity(){
 		if (!worldObj.isBlockIndirectlyGettingPowered(this.xCoord, this.yCoord, this.zCoord)){
-			addEntityYSpeed(AdvancedKinetics.gravityInverterRange.getInt(),0.15D);
+			pullEntities(AdvancedKinetics.gravityWellRange.getInt(),0.05D);
 		}
 	}
 	
-	public void addEntityYSpeed(int range, double speed){
+	public void pullEntities(int range, double speed){
 		AxisAlignedBB area = AxisAlignedBB.getAABBPool().getAABB((double)this.xCoord, (double)this.yCoord, (double)this.zCoord, (double)(this.xCoord + 1), (double)(this.yCoord + 1), (double)(this.zCoord + 1)).expand(range, range, range);
 		List inarea = this.worldObj.getEntitiesWithinAABB(Entity.class, area);
 		Iterator iterator = inarea.iterator();
 		Entity entity;
 		
 		while (iterator.hasNext())
-        {
+        { //TODO: make this stronger at center instead of edge
 			entity = (Entity)iterator.next();
-            entity.motionY+=speed;
+			
+			entity.motionX+=speed*(this.xCoord-entity.posX);
+			entity.motionY+=speed*(this.yCoord-entity.posY);
+			entity.motionZ+=speed*(this.zCoord-entity.posZ);
         }
+	}
+	
+	private double getLargest(double x, double y, double z) {
+		double max;
+		max=x;
+		if(y>max){max=y;}
+		if(z>max){max=z;}
+		return max;
 	}
 }
